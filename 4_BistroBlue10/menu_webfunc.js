4266595d64d69a64f82d2a3db23535ca79084b4a
tijndegit <111745658+tijndegit@users.noreply.github.com>
2022-09-22 15:23:58 +0200

    Update to script

--- 4_BistroBlue10/menu_webfunc.js
+++ 4_BistroBlue10/menu_webfunc.js
@@ -4,7 +4,7 @@ let totalPrice = parseFloat(0) // Definieer globaal de totaalprijs.
 function ComputeAmount() {
     document.getElementById("totaalbedrag").innerHTML = "" // Verwijder eventueel eerder berekend bedrag.
     var paragraph = document.createElement("p"); // Maak een tekstregel.
-    var text = document.createTextNode("Te betalen: €" + totalPrice.toFixed(2)); // Definieer de tekst voor in de tekstregel.
+    var text = document.createTextNode("Te betalen: " + totalPrice.toFixed(2) + " euro"); // Definieer de tekst voor in de tekstregel.
     paragraph.appendChild(text); // Zet de text in de regel.
     var element = document.getElementById("totaalbedrag")//Pak de target div.
     element.appendChild(paragraph); // Voeg nieuwe regel toe aan target div.
@@ -30,5 +30,28 @@ function EmptyList() {
      element.appendChild(paragraph); // Voeg nieuwe bulletpoint toe aan target div.
  }
 
-
+function getJSON() {
+    fetch("https://b10bc-weu-httptriggertijn-fa.azurewebsites.net/api/GetMenu")
+        .then(response => response.json())
+        .then((body) => {
+            console.log(body)
+            for (var dish of body) {
+                var menu_txt = dish.value;
+                if (dish.type == "voor") {
+                    document.getElementById("voor").innerHTML += "<input type = 'radio' value='"+menu_txt+"' name = "+dish.name+" id = "+menu_txt+">"
+                    document.getElementById("voor").innerHTML += "<label for ='"+menu_txt+"'>"+menu_txt+"</label><br>"
+                } else if (dish.type == "hoofd") {
+                    document.getElementById("hoofd").innerHTML += "<input type = 'radio' value='"+menu_txt+"' name = "+dish.name+" id = "+menu_txt+">"
+                    document.getElementById("hoofd").innerHTML += "<label for ='"+menu_txt+"'>"+menu_txt+"</label><br>"
+                } else if (dish.type == "na") {
+                    document.getElementById("na").innerHTML += "<input type = 'radio' value='"+menu_txt+"' name = "+dish.name+" id = "+menu_txt+">"
+                    document.getElementById("na").innerHTML += "<label for ='"+menu_txt+"'>"+menu_txt+"</label><br>"
+                } else if (dish.type == "drank") {
+                    document.getElementById("drank").innerHTML += "<input type = 'radio' value='"+menu_txt+"' name = "+dish.name+" id = "+menu_txt+">"
+                    document.getElementById("drank").innerHTML += "<label for ='"+menu_txt+"'>"+menu_txt+"</label><br>"
+                }
+            };
+        });
+    
+}
 
